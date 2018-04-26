76d2d8a07a82ec0c9f8f898ab3031300a9d8c01e
Yerlan Akhmetov <kanzhygaly@gmail.com>
2018-04-26 15:56:38 +0600

    First Commit

---
+++ FeedHandler.py
@@ -0,0 +1,54 @@
+import feedparser
+import re
+
+
+class FeedHandler:
+
+    @staticmethod
+    def parse_feed(url, entries=0):
+        """
+        Parses the given url, returns a list containing all available entries
+        """
+
+        if 1 <= entries <= 10:
+            feed = feedparser.parse(url)
+            return feed.entries[:entries]
+        else:
+            feed = feedparser.parse(url)
+            return feed.entries[:4]
+
+    @staticmethod
+    def is_available(url):
+        """
+        Checks whether the given url provides a news feed. Return True if news are available, else False
+        """
+
+        url_pattern = re.compile("((http(s?))):\/\/.*")
+        if not url_pattern.match(url):
+            return False
+
+        feed = feedparser.parse(url)
+
+        # Check if result is empty
+        if not feed.entries:
+            return False
+        # Check if entries provide updated attribute
+        for post in feed.entries:
+            if not hasattr(post, "updated"):
+                return False
+        return True
+
+    @staticmethod
+    def format_url_string(string):
+        """
+        Formats a given url as string so it matches http(s)://adress.domain.
+        This should be called before parsing the url, to make sure it is parsable
+        """
+
+        string = string.lower()
+
+        url_pattern = re.compile("((http(s?))):\/\/.*")
+        if not url_pattern.match(string):
+            string = "http://" + string
+
+        return string
---
+++ bot.py
@@ -0,0 +1,65 @@
+import datetime
+
+import FeedHandler
+from BotHandler import BotHandler
+
+greet_bot = BotHandler('552105148:AAH4hH232QZy7aOJ8IJyaXvc_L2Gq9t1Eh8')
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')
+wod = ('что там сегодня', 'тренировка', 'треня', 'wod')
+now = datetime.datetime.now()
+
+
+def main():
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        if last_update is not None:
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
+
+            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+                greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+                today += 1
+
+            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+                greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+                today += 1
+
+            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+                greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+                today += 1
+
+            elif last_chat_text.lower() in wod:
+                url = 'http://comptrain.co/individuals/home/'
+
+                # Check if argument matches url format
+                if not FeedHandler.is_available(url):
+                    message = "Sorry! It seems like '" + str(url) + "' doesn't provide an RSS news feed.. Have you " \
+                                                                    "tried another URL from that provider? "
+                    greet_bot.send_message(last_chat_id, message)
+                    return
+
+                args_count = 4
+
+                entries = FeedHandler.parse_feed(url, args_count)
+                for entry in entries:
+                    message = "[" + url[1] + "] <a href='" + \
+                              entry.link + "'>" + entry.title + "</a>"
+                    print(message)
+
+            new_offset = last_update_id + 1
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
