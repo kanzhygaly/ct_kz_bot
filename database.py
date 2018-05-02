0e236385d8394626dbc64b312ffd83397ea3a290
Yerlan Akhmetov <kanzhygaly@gmail.com>
2018-05-02 17:42:42 +0600

    Postgres DB

--- database.py
+++ database.py
@@ -23,24 +23,24 @@ class Database:
         self.connection.commit()
 
     def add_user(self, user_id, name, surname, lang):
-        self.cursor.execute("INSERT INTO users (user_id, name, surname, lang) VALUES (%d, %s, %s, %s)",
+        self.cursor.execute("INSERT INTO users (user_id, name, surname, lang) VALUES (%s, %s, %s, %s)",
                             (user_id, name, surname, lang))
         self.connection.commit()
 
     def is_user_exist(self, user_id):
-        self.cursor.execute("SELECT * FROM users WHERE user_id=%d", (user_id,))
+        self.cursor.execute("SELECT * FROM users WHERE user_id=%s", (user_id,))
         return bool(self.cursor.fetchone())
 
     def add_subscriber(self, user_id):
-        self.cursor.execute("INSERT INTO subscribers (user_id) VALUES (%d)", (user_id,))
+        self.cursor.execute("INSERT INTO subscribers (user_id) VALUES (%s)", (user_id,))
         self.connection.commit()
 
     def is_subscriber(self, user_id):
-        self.cursor.execute("SELECT * FROM subscribers WHERE user_id=%d", (user_id,))
+        self.cursor.execute("SELECT * FROM subscribers WHERE user_id=%s", (user_id,))
         return bool(self.cursor.fetchone())
 
     def unsubscribe(self, user_id):
-        self.cursor.execute("DELETE FROM subscribers WHERE user_id=%d", (user_id,))
+        self.cursor.execute("DELETE FROM subscribers WHERE user_id=%s", (user_id,))
 
     def get_all_subscribers(self):
         self.cursor.execute("SELECT * FROM subscribers")
