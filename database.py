fc3113f5b81693553a8360a57e55452c0029eac4
Yerlan Akhmetov <kanzhygaly@gmail.com>
2018-05-02 17:34:51 +0600

    Postgres DB

--- database.py
+++ database.py
@@ -12,35 +12,35 @@ class Database:
         self.cursor = self.connection.cursor()
 
     def init_tables(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, user_id VARCHAR(50),"
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, user_id BIGINT,"
                             "name VARCHAR(100), surname VARCHAR(100), lang VARCHAR(10));")
 
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS subscribers (id serial PRIMARY KEY, user_id VARCHAR(50));")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS subscribers (id serial PRIMARY KEY, user_id BIGINT);")
 
         self.cursor.execute("CREATE TABLE IF NOT EXISTS wod (id serial PRIMARY KEY, wod_date date, title VARCHAR(150),"
-                            "description text, result VARCHAR(150), user_id VARCHAR(50));")
+                            "description text, result VARCHAR(150), user_id BIGINT);")
 
         self.connection.commit()
 
     def add_user(self, user_id, name, surname, lang):
-        self.cursor.execute("INSERT INTO users (user_id, name, surname, lang) VALUES (%s, %s, %s, %s)",
+        self.cursor.execute("INSERT INTO users (user_id, name, surname, lang) VALUES (%d, %s, %s, %s)",
                             (user_id, name, surname, lang))
         self.connection.commit()
 
     def is_user_exist(self, user_id):
-        self.cursor.execute("SELECT * FROM users WHERE user_id=%s", user_id)
+        self.cursor.execute("SELECT * FROM users WHERE user_id=%d", user_id)
         return bool(self.cursor.fetchone())
 
     def add_subscriber(self, user_id):
-        self.cursor.execute("INSERT INTO subscribers (user_id) VALUES (%s)", user_id)
+        self.cursor.execute("INSERT INTO subscribers (user_id) VALUES (%d)", user_id)
         self.connection.commit()
 
     def is_subscriber(self, user_id):
-        self.cursor.execute("SELECT * FROM subscribers WHERE user_id=%s", user_id)
+        self.cursor.execute("SELECT * FROM subscribers WHERE user_id=%d", user_id)
         return bool(self.cursor.fetchone())
 
     def unsubscribe(self, user_id):
-        self.cursor.execute("DELETE FROM subscribers WHERE user_id=%s", user_id)
+        self.cursor.execute("DELETE FROM subscribers WHERE user_id=%d", user_id)
 
     def get_all_subscribers(self):
         self.cursor.execute("SELECT * FROM subscribers")
