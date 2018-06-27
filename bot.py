ccca8889f91eada4c58cc4e5144edb603db636bd
Yerlan Akhmetov <kanzhygaly@gmail.com>
2018-06-27 19:46:56 +0600

    warm up

--- bot.py
+++ bot.py
@@ -494,7 +494,6 @@ async def set_location(message: types.Message):
 @dp.message_handler(func=lambda message: message.text.lower() in warmup_requests)
 async def view_warmup(message: types.Message):
     today = datetime.now().date()
-    print(today)
     result = await wod_db.get_warmup(today)
     if result:
         await bot.send_message(message.chat.id, result)
--- test/get_wod_test.py
+++ test/get_wod_test.py
@@ -3,9 +3,6 @@ import re
 
 from bsoup_spider import BSoupParser
 
-msg = "/wod_results\n\n" \
-      "/add_wod_result"
-
 now = datetime.now()
 
 url = 'http://comptrain.co/individuals/home'
@@ -16,7 +13,9 @@ num = re.sub(r'\D', "", parser.get_wod_date())
 wod_date = datetime.strptime(num, '%m%d%y')
 print(wod_date)
 
-if wod_date.date().__eq__(now.date()):
+today = now.date()
+
+if wod_date.date().__eq__(today):
     title = parser.get_wod_date()
     regional_part = parser.get_regional_wod()
     open_part = parser.get_open_wod()
@@ -28,10 +27,40 @@ if wod_date.date().__eq__(now.date()):
     open_text = open_text[4:]
 
     if not reg_text.startswith("regionalsathletesrest") and not open_text.startswith("openathletesrest"):
-        print(title + "\n\n" + description + "\n\n" + msg)
+        # print(title + "\n\n" + description + "\n\n" + msg)
+        pass
     else:
         print(reg_text + "\n\n" + open_text)
 else:
     print("Комплекс еще не вышел.\nСорян :(")
 
+weekday = {
+    '0': 'sunday',
+    '1': 'monday',
+    '2': 'tuesday',
+    '3': 'wednesday',
+    '4': 'thursday',
+    '5': 'friday',
+    '6': 'saturday'
+}
+index = today.strftime("%w")
+target = today.strftime("%m-%d-%y").lstrip("0").replace("0", "")
+url = f'http://comptrain.co/individuals/workout/{weekday.get(index)}-·-{target}'
+try:
+    parser = BSoupParser(url=url)
+    regional_part = parser.get_regional_wod()
+    open_part = parser.get_open_wod()
+    description = regional_part + "\n" + open_part
 
+    reg_text = (''.join(regional_part.split())).lower()
+    reg_text = reg_text[4:]
+    open_text = (''.join(open_part.split())).lower()
+    open_text = open_text[4:]
+
+    if not reg_text.startswith("regionalsathletesrest") and not open_text.startswith("openathletesrest"):
+        print(title + "\n\n" + description)
+    else:
+        print(reg_text + "\n\n" + open_text)
+except Exception as e:
+    print(e)
+    print("Комплекс еще не вышел.\nСорян :(")
