126d9c1cd01e58a19500afd35681c8c7276e58d2
Yerlan Akhmetov <kanzhygaly@gmail.com>
2020-06-11 00:17:00 +0300

    search

--- bot.py
+++ bot.py
@@ -491,7 +491,7 @@ async def view_wod_results_callback(callback_query: types.CallbackQuery):
         msg = "На этот день нет результатов"
 
     wod_day = await wod_db.get_wod_day(wod_id)
-    msg = wod_day.strftime("%d %B %Y") + "\n" + msg
+    msg = wod_day.strftime("%d %B %Y") + "\n\n" + msg
 
     await bot.edit_message_text(text=msg, chat_id=chat_id, message_id=callback_query.message.message_id,
                                 parse_mode=ParseMode.MARKDOWN)
