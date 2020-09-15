9400994a191c3dac7fcfe9b6c38169f56e4cb95b
Yerlan Akhmetov <kanzhygaly@gmail.com>
2020-09-15 22:11:38 +0300

    refactoring

--- bot/service/notification_service.py
+++ bot/service/notification_service.py
@@ -1,4 +1,4 @@
-from aiogram import types
+from aiogram import types, Bot
 from aiogram.utils.emoji import emojize
 from aiogram.utils.exceptions import UserDeactivated
 
@@ -8,7 +8,7 @@ from bot.service.wod_result_service import has_wod_result
 from bot.service.wod_service import get_today_wod, get_today_wod_id
 
 
-async def send_wod_to_all_subscribers(bot) -> None:
+async def send_wod_to_all_subscribers(bot: Bot) -> None:
     subscribers = await subscriber_db.get_all_subscribers()
 
     msg, wod_id = await get_today_wod()
@@ -17,16 +17,19 @@ async def send_wod_to_all_subscribers(bot) -> None:
         msg += "\n\n/add - записать/изменить результат за СЕГОДНЯ\n" \
                "/results - посмотреть результаты за СЕГОДНЯ"
 
-    print(f'Sending WOD to {len(subscribers)} subscribers')
+    count = 0
     for sub in subscribers:
         try:
             await bot.send_message(sub.user_id, msg)
+            count += 1
         except UserDeactivated:
             print(f'User {sub.user_id} is deactivated, deleting him from subscribers')
             await subscriber_db.unsubscribe(sub.user_id)
 
+    print(f'WOD has been sent to {count} subscribers')
 
-async def notify_all_subscribers_to_add_result(bot) -> None:
+
+async def notify_all_subscribers_to_add_result(bot: Bot) -> None:
     try:
         wod_id = await get_today_wod_id()
 
