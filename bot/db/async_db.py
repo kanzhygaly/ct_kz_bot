952dcd5ab67d1b6ab965e48ead1466d9078ffcd3
Yerlan Akhmetov <kanzhygaly@gmail.com>
2020-10-03 23:15:07 +0300

    pathlib

--- bot/db/async_db.py
+++ bot/db/async_db.py
@@ -1,8 +1,9 @@
 import os
+from pathlib import Path
 
 from asyncpg_simpleorm import PoolManager, AsyncModel
 
-from bot.constants.config_vars import DATABASE_URL
+from bot.constants.config_vars import DATABASE_URL, DB_SCRIPT
 from bot.util.parser_util import database_url_parse
 
 credentials = database_url_parse(os.environ[DATABASE_URL])
@@ -23,24 +24,10 @@ class Entity(AsyncModel, connection=manager):
 
 
 async def create_all_tables(connection) -> None:
-    await connection.execute('''
-            CREATE TABLE IF NOT EXISTS users (id uuid PRIMARY KEY, user_id BIGINT,
-            name VARCHAR(100), surname VARCHAR(100), lang VARCHAR(10), admin BOOL DEFAULT false);
-
-            CREATE TABLE IF NOT EXISTS subscribers (id uuid PRIMARY KEY, user_id BIGINT UNIQUE);
-
-            CREATE TABLE IF NOT EXISTS wod (id uuid PRIMARY KEY, wod_day date, title VARCHAR(150),
-            description TEXT, warmup TEXT);
-
-            CREATE TABLE IF NOT EXISTS wod_result (id uuid PRIMARY KEY, wod_id uuid not null,
-            user_id BIGINT, result TEXT, sys_date TIMESTAMP);
-
-            CREATE TABLE IF NOT EXISTS benchmark (id uuid PRIMARY KEY, title VARCHAR(150),
-            description TEXT, result_type VARCHAR(50));
-
-            CREATE TABLE IF NOT EXISTS benchmark_result (id uuid PRIMARY KEY, benchmark_id uuid not null,
-            wod_day date, user_id BIGINT, result VARCHAR(200), sys_date TIMESTAMP, link VARCHAR(100));
-            
-            CREATE TABLE IF NOT EXISTS location (user_id BIGINT PRIMARY KEY, longitude FLOAT,
-            latitude FLOAT, locale VARCHAR(10), tz VARCHAR(200));
-        ''')
+    p = Path('../resources/db/create_all_tables.sql')
+    if p.exists() and p.is_file():
+        print(p.read_text())
+        switch = os.environ[DB_SCRIPT]
+        print(switch)
+        if switch and switch == 'enabled':
+            await connection.execute(p.read_text())
