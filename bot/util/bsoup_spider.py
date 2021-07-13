a56ed4176f820cc2af800356787b3ded2a5c4110
Yerlan Akhmetov <kanzhygaly@gmail.com>
2021-07-13 23:08:29 +0300

    user_agent

--- bot/util/bsoup_spider.py
+++ bot/util/bsoup_spider.py
@@ -1,20 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+from bot.util import get_user_agent
 from bot.util.parser_util import parse_wod_content
 
 
 class BSoupParser:
     def __init__(self, url: str):
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        page = requests.get(url, headers=headers)
+        headers = {'User-Agent': get_user_agent()}
+        try:
+            page = requests.get(url, headers=headers)
+            soup = BeautifulSoup(page.text, 'html.parser')
+        except Exception as e:
+            print('Error: ', str(e))
 
-        soup = BeautifulSoup(page.text, 'html.parser')
         post = soup.find('div', id='wod')
-
         header = post.find('h5').get_text()
-        self.wod_date = " ".join(header.split())
 
+        self.wod_date = " ".join(header.split())
         self.wod_text = parse_wod_content(post)
 
     def get_wod_date(self) -> str:
